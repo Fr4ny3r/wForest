f7fb3ebed4036c6a6c1db9fb2334fd90a3fe02aa
Fr4ny3r <guevarafranyer09@gmail.com>
2025-11-07 14:36:06 -0400

    v1

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -2,8 +2,11 @@
 
 function CTA () {
     return (
-        <main className="bg-accent-light py-24 px-4 text-center">
-    <div className="max-w-4xl mx-auto">
+        <section
+            className="w-screen h-[700px] my-10 px-4 relative text-center"
+        >
+            <img src="https://images.unsplash.com/photo-1628075264522-541fc688e9d9?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170" className="absolute top-0 left-1/2 -translate-x-1/2 h-full max-w-[1800px]"/>
+    <div className="flex flex-col items-center justify-center h-full py-20 z-1000 bg-[var(--color-bg)]/40 backdrop-blur-[4px] relative">
         
         <h2 className="text-5xl font-extrabold text-dark-bg mb-6 leading-tight">
             Be the Force for Nature
@@ -13,7 +16,7 @@ function CTA () {
             Every trail protected, every animal rescued, starts with a single step. Join our mission and make a tangible impact today.
         </p>
         
-        <a href="#" className="inline-block bg-primary-dark text-text-light font-bold text-lg px-12 py-4 rounded-full shadow-lg transition duration-300 hover:bg-opacity-90 hover:shadow-xl transform hover:scale-105">
+        <a href="#" className="font-bold inline-block bg-[var(--color-primary)] font-bold text-lg px-12 py-4 rounded-full shadow-lg transition duration-300 hover:bg-opacity-90 hover:shadow-xl transform hover:scale-105">
             Donate and Protect
         </a>
         
@@ -23,7 +26,7 @@ function CTA () {
             </a>
         </p>
     </div>
-</main>
+</section>
     )
 }
 
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 function Footer() {
   return (
-<footer className="bg-dark-bg text-text-light py-12 px-4 md:px-8">
+<footer className="bg-dark-bg text-text-light py-12 mt-40 px-4 md:px-8">
     <div className="max-w-6xl mx-auto">
         
         <div className="grid grid-cols-1 md:grid-cols-4 gap-10 border-b border-primary-light/20 pb-8">
@@ -44,10 +44,16 @@ function Footer() {
                     Phone: (123) 456-7890
                 </p>
                 
-                <div className="flex space-x-4">
-                    <a href="#" className="text-primary-light hover:text-accent-light transition-colors">FB</a>
-                    <a href="#" className="text-primary-light hover:text-accent-light transition-colors">TW</a>
-                    <a href="#" className="text-primary-light hover:text-accent-light transition-colors">IG</a>
+                <div className="flex justify-center space-x-4">
+                    <a href="#" className="text-primary-light hover:text-accent-light transition-colors">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-brand-facebook"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" /></svg>
+                    </a>
+                    <a href="#" className="text-primary-light hover:text-accent-light transition-colors">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-brand-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 4l11.733 16h4.267l-11.733 -16z" /><path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" /></svg>
+                    </a>
+                    <a href="#" className="text-primary-light hover:text-accent-light transition-colors">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-brand-instagram"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 8a4 4 0 0 1 4 -4h8a4 4 0 0 1 4 4v8a4 4 0 0 1 -4 4h-8a4 4 0 0 1 -4 -4z" /><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path d="M16.5 7.5v.01" /></svg>
+                    </a>
                 </div>
             </div>
         </div>
